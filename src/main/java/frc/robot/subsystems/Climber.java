57495608fd168f9b019a45c3150c9f5ef42ad0f0
5013-Programmer <46497490+5013-Programmer@users.noreply.github.com>
2022-01-10 16:26:28 -0600

    add classes

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {
-  /** Creates a new Shooter. */
-  public Shooter() {}
+public class Climber extends SubsystemBase {
+  /** Creates a new Climber. */
+  public Climber() {}
 
   @Override
   public void periodic() {
