4c08f75ff9b856d52cf59624968c36dbf059a451
bmongar <bradmongar@gmail.com>
2022-01-10 16:42:01 -0600

    Update shooter.java

--- src/main/java/frc/robot/subsystems/shooter.java
+++ src/main/java/frc/robot/subsystems/shooter.java
@@ -7,8 +7,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-
-  /** Creates a new shooter. */
+  /** Creates a new Shooter. */
   public Shooter() {}
 
   @Override
