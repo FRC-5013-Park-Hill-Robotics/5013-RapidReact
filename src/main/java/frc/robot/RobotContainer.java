69ebb125fea62d8ab12dc202eb7c59e66ea4f5ce
5013-Programmer <91989833+adira5013@users.noreply.github.com>
2022-01-12 14:30:43 -0600

    Revert "Merge branch 'main' into Adira"
    This reverts commit c39cb8f9dce8e8c69fd4c0a80c41779845df8983, reversing
    changes made to 3f576a3487e748046c74693321266f6b885a1f23.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,7 @@ import frc.robot.commands.GamepadDrive;
 import frc.robot.commands.TurnToAngleCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -53,10 +53,10 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         // Back button zeros the gyroscope
-        new Button(m_controller::getBackButton)
+        new JoystickButton(m_controller, LogitechController.Button.kBack.value)
                 // No requirements because we don't need to interrupt anything
                 .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
-        new Button(m_controller::getAButton)
+        new JoystickButton(m_controller, LogitechController.Button.kA.value)
                 .whenPressed(new TurnToAngleCommand(m_drivetrainSubsystem, Math.PI));
     }
 
