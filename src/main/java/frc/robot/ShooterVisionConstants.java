c5d47d75063a509e6a9c2c062670581e22ca6bd7
bmongar <bradmongar@gmail.com>
2022-01-12 06:23:43 -0600

    Cleanup some errors

---
+++ src/main/java/frc/robot/ShooterVisionConstants.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot ;
+
+/** Add your docs here. */
+public final class ShooterVisionConstants {
+    public static final double CAMERA_ANGLE = 0;
+    public static final double CAMERA_HEIGHT = 0;
+    public static final double TARGET_HEIGHT = 0;
+    public static final int TARGET_PIPELINE = 0;
+    public static final int DEFAULT_PIPELINE = 1;
+    public static final int DRIVE_PIPELINE = 2;
+    public static final int LED_ON = 3;
+    public static final int LED_OFF = 1;
+    public static final double TURN_TO_TARGET_TOLERANCE = 1.5;
+    public static final double RANGE_TOO_CLOSE =4;
+    public static final double RANGE_TOO_FAR=-9;
+    public static final double RANGE_PRIME_START=0;
+    public static final double RANGE_PRIME_END=-2;
+}
