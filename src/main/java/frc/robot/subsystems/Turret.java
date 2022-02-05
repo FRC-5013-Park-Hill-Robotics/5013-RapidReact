6be0f056edbaf926fa471e8d9e145eef8f3de608
tylurr <tylerpokemon3@gmail.com>
2022-02-05 10:03:05 -0600

    turret update

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -6,16 +6,29 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.TurretConstants.*;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Turret extends SubsystemBase {
     private WPI_TalonSRX motor = new WPI_TalonSRX(TURRET_MOTOR);
 	/** Creates a new Turret. */
 	public Turret() {
-
+        super();
 	}
-
+    public double getCurrentAngle() {
+        return 1;
+        //max 270, min0?
+    }
+    
+    public double setDesiredAngle() {
+        Rotation2d angle = Rotation2d.fromDegrees(getCurrentAngle());
+        return 1; 
+        
+    }
+    
 	@Override
 	public void periodic() {
 		// This method will be called once per scheduler run
+        //pid controller
+        //commands
 	}
 }
