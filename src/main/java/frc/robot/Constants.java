5406c6ee7f68ba7812053daf2c01b4283be2b2a0
bmongar <bradmongar@gmail.com>
2022-02-19 16:12:28 -0600

    Update Constants.java
    Forgot decimal point

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -45,7 +45,7 @@ public final class Constants {
              * The left-to-right distance between the drivetrain wheels Should be measured
              * from center to center.
              */
-            public static final double TRACKWIDTH_METERS = 585;
+            public static final double TRACKWIDTH_METERS = .585;
             /**
              * The front-to-back distance between the drivetrain wheels. Should be measured
              * from center to center.
