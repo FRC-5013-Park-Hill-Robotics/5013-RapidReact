da06938d71123653d81673a69c8a8ad278cc9ae9
bmongar <bradmongar@gmail.com>
2022-02-19 14:07:04 -0600

    Revert "Update Turret.java"
    This reverts commit 8544a4d974b938e984d863dfb2cef1757b007b5a.

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -5,21 +5,22 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.TurretConstants.*;
-import frc.robot.trobot5013lib.LinearServo;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Servo;
+import frc.robot.subsystems.ShooterVision;
 
 public class Turret extends SubsystemBase {
     private WPI_TalonSRX motor = new WPI_TalonSRX(TURRET_MOTOR);
-    private LinearServo servo = new LinearServo(SERVO_LEFT_ID,140,32);
-
+    private Servo leftServo = new Servo(SERVO_LEFT_ID);
+    private Servo rightServo = new Servo(SERVO_RIGHT_ID);
 
     double desiredAngle = 0;
     
 	/** Creates a new Turret. */
 	public Turret() {
-		servo.setPosition(0);
 	}
     //Gets the current angle of the Talon SRX.
     public double getCurrentAngle() {
@@ -36,23 +37,13 @@ public class Turret extends SubsystemBase {
         motor.set(speed);
     }
     
-
     //Sets the pulse width (height) of the left/right servos
     public void setHeight(double height) {
-        servo.setHeight(height);
+        leftServo.set(height);
+        rightServo.set(height);
     }
-
-	public void up(double increment){
-		setHeight(servo.getHeight() + increment);
-	}
-
-	public void down(double increment){
-		setHeight(servo.getHeight() - increment);
-	}
-
 	@Override
 	public void periodic() {
-		SmartDashboard.putNumber("Height", servo.getHeight());
 		// This method will be called once per scheduler run
         //pid controls
         //motor.set
