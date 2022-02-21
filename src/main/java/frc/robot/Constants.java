e8739113b2e6d8703031067324404216f9926057
bmongar <bradmongar@gmail.com>
2022-02-21 16:29:15 -0600

    Pneumatics
    MAke Pneumatics work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -128,7 +128,7 @@ public final class Constants {
     public static final class IntakeConstants {
         public static final int INTAKE_MOTOR = 20;
         public static final int DROP_INTAKE_SOLENOID_CHANNEL = 0;
-        public static final int RAISE_INTAKE_SOLENOID_CHANNEL = 0;
+        public static final int RAISE_INTAKE_SOLENOID_CHANNEL = 1;
         public static final int ROLLER_SERVO = 0;
     }
 
