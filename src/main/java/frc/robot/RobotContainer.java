9aba227dfc48a78db100c1b6b0175fce80dc9335
bmongar <bradmongar@gmail.com>
2022-03-03 17:16:11 -0600

    remove resolution
    Axon util resolution parse remove

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -52,7 +52,7 @@ public class RobotContainer {
 	private ShooterVision m_shooterVision = new ShooterVision();
 	private Conveyor m_conveyor = new Conveyor(this);
 	private CargoShooter m_shooter = new CargoShooter(m_conveyor);
-	private IntakeVision m_IntakeVision;// = new IntakeVision(this);
+	private IntakeVision m_IntakeVision = new IntakeVision(this);
 	private Intake m_intake = new Intake(m_conveyor,this);
 	private Climber m_Climber = new Climber();
 	
@@ -97,8 +97,8 @@ public class RobotContainer {
 		new Button(m_controller::getLeftTriggerButton).whileHeld(new TeleopTurnToTargetCommand(m_drivetrainSubsystem,m_shooterVision, m_shooter,
 			 m_turret, m_conveyor::isAllianceBallNext,m_controller::getLeftY, m_controller::getLeftY, m_controller::getRightTriggerAxis));
 		new Button(m_controller::getLeftBumper).whenPressed(new Fire(m_shooter, m_conveyor)).whenReleased(m_shooter::stopFiring);
-		new Button(m_controller::getAButton).whileHeld(new Fetch(m_drivetrainSubsystem, m_IntakeVision,m_controller::getLeftX,m_controller::getLeftY,
-				 m_controller::getRightTriggerAxis));
+//	new Button(m_controller::getAButton).whileHeld(new Fetch(m_drivetrainSubsystem, m_IntakeVision,m_controller::getLeftX,m_controller::getLeftY,
+//				 m_controller::getRightTriggerAxis));
 				 
 		new Button(m_controller::getDPadUp).whenPressed(new InstantCommand(() -> m_turret.up(10)));
 		new Button(m_controller::getDPadDown).whenPressed(new InstantCommand(() -> m_turret.down(10)));
